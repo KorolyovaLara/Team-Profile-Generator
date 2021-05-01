e497af350d56a1051e62b60d7780246859af3aa6
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-05-01 13:01:36 +1000

    Completed questions for Manager

--- assets/index.js
+++ assets/index.js
@@ -9,3 +9,63 @@ const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+// Selector for Member of the Team
+
+const chooseMember = [
+    {
+        type: "list",
+        name: "teamMember",
+        message: "Please choose the role for the employee",
+        choices: ["Manager", "Engineer", "Intern"],
+    }
+];
+
+const questions = {
+    Manager: [
+        {
+            type: "input",
+            name: "name",
+            message: "What is the manager's name?",
+            validate: (value) => {
+               if (value) {return true}
+               else {return "Please enter manager's name."} 
+            },
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the manager's id?",
+            validate: (value) => {
+               if (value) {return true}
+               else {return "Please enter manager's id."} 
+            },
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the manager's email?",
+            validate: (value) => {
+               if (emailValidator.validate(value)) {return true}
+               else {return "Please enter valid email address."} 
+            },
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office phone number?",
+            validate: (value) => {
+               if (value) {return true}
+               else {return "Please enter manager's office phone number."} 
+            },
+        },
+        {
+            type: "list",
+            name: "addNew",
+            message: "Do you want to add another employee?",
+            choices: ["yes", "no"],
+        },
+    ],
+    Engineer: [],
+    Intern: [],
+};
