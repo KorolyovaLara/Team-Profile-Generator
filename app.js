a5d3ce676e5d7b709756f3ab89d5f7f7673e36f4
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-05-10 22:23:25 +1000

    completed the code

--- app.js
+++ app.js
@@ -19,91 +19,118 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const allMembers = [];
 const log = new Logger();
 
-// Selector for Member of the Team
-const chooseMember = [
+// Intro for Application
+const intoScreen = {
+  type: "list",
+  name: "welcomeScreen",
+  message: `
+    === Welcome to the Team Profile Generator Application ===
+
+    This program will allow the user to build a team and to create an HTML that will display the information for the team, including details of each member.
+
+    Do you wish to continue with this application?
+    `,
+  choices: ["Yes, Start Building Team", "No, Close Application"],
+};
+
+// Manager Questions
+const addManager = [
   {
-    type: "list",
-    name: "role",
-    message: "Please choose the role for the employee",
-    // function to allow only one manager to be created
-    choices: () => {
-      if (allMembers.some((member) => member.role === "Manager")) {
-        return ["Engineer", "Intern"];
+    type: "input",
+    name: "name",
+    message: "What is the manager's name?",
+    validate: function (txt) {
+      const letters = /^[A-Za-z]+$/.test(txt);
+      if (letters) {
+        return true;
       } else {
-        return ["Manager", "Engineer", "Intern"];
+        log.red(
+          `Please enter a valid name that does not include anything other than letters`
+        );
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the manager's id?",
+    validate: function (num) {
+      const number = /^[0-9]+$/.test(num);
+      if (number) {
+        return true;
+      } else {
+        log.red(
+          `Please enter a valid ID that does not include anything other than number`
+        );
+        return false;
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the manager's email?",
+    validate: (value) => {
+      if (emailValidator.validate(value)) {
+        return true;
+      } else {
+        return "Please enter valid email address.";
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "What is the manager's office phone number?",
+    validate: function (num) {
+      const number = /^[0-9]+$/.test(num);
+      if (number) {
+        return true;
+      } else {
+        log.red(
+          `Please enter a valid phone number that does not include anything other than number`
+        );
+        return false;
       }
     },
   },
 ];
 
-// All questions
+// Add Engineer and Intern
+const addOthers = {
+  type: "list",
+  name: "addMembers",
+  message: `Would you like to add another team member to this team? 
+  => Select "YES" to add an Engineer or Intern team member or 
+  => Select "NO" if no additional team members need to be added.`,
+  choices: ["YES", "NO"],
+};
+
+// Selector of the Role for team member
+const chooseRole = {
+  type: "list",
+  name: "role",
+  message: "Please choose the role for the employee",
+  choices: ["Engineer", "Intern"],
+};
+
+// Engineer and Intern Questions
 const questions = {
-  Manager: [
-    {
-      type: "input",
-      name: "name",
-      message: "What is the manager's name?",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please enter manager's name.";
-        }
-      },
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is the manager's id?",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please enter manager's id.";
-        }
-      },
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is the manager's email?",
-      validate: (value) => {
-        if (emailValidator.validate(value)) {
-          return true;
-        } else {
-          return "Please enter valid email address.";
-        }
-      },
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "What is the manager's office phone number?",
-      validate: (value) => {
-        if (value) {
-          return true;
-        } else {
-          return "Please enter manager's office phone number.";
-        }
-      },
-    },
-    {
-      type: "list",
-      name: "addNew",
-      message: "Do you want to add another employee?",
-      choices: ["Yes", "No"],
-    },
-  ],
   Engineer: [
     {
       type: "input",
       name: "name",
       message: "What is the engineer's name?",
-      validate: (value) => {
-        if (value) {
+      validate: function (txt) {
+        const letters = /^[A-Za-z]+$/.test(txt);
+        if (letters) {
           return true;
         } else {
-          return "Please enter engineer's name.";
+          log.red(
+            `Please enter a valid name that does not include anything other than letters`
+          );
+          return false;
         }
       },
     },
@@ -111,11 +138,15 @@ const questions = {
       type: "input",
       name: "id",
       message: "What is the engineer's id?",
-      validate: (value) => {
-        if (value) {
+      validate: function (num) {
+        const number = /^[0-9]+$/.test(num);
+        if (number) {
           return true;
         } else {
-          return "Please enter engineer's id.";
+          log.red(
+            `Please enter a valid ID that does not include anything other than number`
+          );
+          return false;
         }
       },
     },
@@ -143,23 +174,21 @@ const questions = {
         }
       },
     },
-    {
-      type: "list",
-      name: "addNew",
-      message: "Do you want to add another employee?",
-      choices: ["yes", "no"],
-    },
   ],
   Intern: [
     {
       type: "input",
       name: "name",
       message: "What is the intern's name?",
-      validate: (value) => {
-        if (value) {
+      validate: function (txt) {
+        const letters = /^[A-Za-z]+$/.test(txt);
+        if (letters) {
           return true;
         } else {
-          return "Please enter intern's name.";
+          log.red(
+            `Please enter a valid name that does not include anything other than letters`
+          );
+          return false;
         }
       },
     },
@@ -167,11 +196,15 @@ const questions = {
       type: "input",
       name: "id",
       message: "What is the intern's id?",
-      validate: (value) => {
-        if (value) {
+      validate: function (num) {
+        const number = /^[0-9]+$/.test(num);
+        if (number) {
           return true;
         } else {
-          return "Please enter intern's id.";
+          log.red(
+            `Please enter a valid ID that does not include anything other than number`
+          );
+          return false;
         }
       },
     },
@@ -199,35 +232,15 @@ const questions = {
         }
       },
     },
-    {
-      type: "list",
-      name: "addNew",
-      message: "Do you want to add another employee?",
-      choices: ["yes", "no"],
-    },
   ],
-};
-
-// Intro for Application
-const intoScreen = {
-  type: "list",
-  name: "welcomeScreen",
-  message: `
-    === Welcome to the Team Profile Generator Application ===
-
-    This program will allow the user to build a team and to create an HTML that will display the information for the team, including details of each member.
-
-    Do you wish to continue with this application?
-    `,
-  choices: ["Yes, Start Building Team", "No, Close Application"],
 };
 
 // Initial function to start the application
 function startApp() {
   inquirer.prompt(intoScreen).then((runApp) => {
     if (runApp.welcomeScreen === "Yes, Start Building Team") {
-      log.cyan("Please submit information for Manager");
-      addNewEmployee();
+      log.cyan("Please submit information for Manager of the Team");
+      askManager();
     } else {
       log.magenta(`
         === Application Closed ===
@@ -236,27 +249,39 @@ function startApp() {
   });
 }
 
+// Function to create the team manager and then call the function to start building the team size
+function askManager() {
+  inquirer.prompt(addManager).then((answer) => {
+    const manager = new Manager(
+      answer.name,
+      answer.id,
+      answer.email,
+      answer.officeNumber
+    );
+    // add details to team array
+    allMembers.push(manager);
+    addTeamMembers();
+  });
+}
+
+// Function to choose the type of team member (engineer or intern) and prompt questions to build additional class constructors.
+function addTeamMembers() {
+  inquirer.prompt(addOthers).then((teamSize) => {
+    // By choosing yes, you can add another team member to the array. This recals the addNewEmployee funciton which goes throught the questions to add a new team member to the array
+    if (teamSize.addMembers === "YES") {
+      addNewEmployee();
+    }
+    if (teamSize.addMembers === "NO") {
+      //If no more members need to be added, then the application is ended by choosing No and then the file is written to the HTML template
+      generateHTML(allMembers);
+    }
+  });
+}
+
 // Function to add new team members
 function addNewEmployee() {
-  inquirer.prompt(chooseMember).then((answer) => {
-    if (answer.role === "Manager") {
-      inquirer.prompt(questions.Manager).then((answer) => {
-        // save employee details
-        const manager = new Manager(
-          answer.name,
-          answer.id,
-          answer.email,
-          answer.officeNumber
-        );
-        // add details to team array
-        allMembers.push(manager);
-        if (answer.addNew === "Yes") {
-          addNewEmployee();
-        } else {
-          generateHTML();
-        }
-      });
-    } else if (answer.role === "Engineer") {
+  inquirer.prompt(chooseRole).then((answer) => {
+    if (answer.role === "Engineer") {
       inquirer.prompt(questions.Engineer).then((answer) => {
         // save employee details
         const engineer = new Engineer(
@@ -267,11 +292,7 @@ function addNewEmployee() {
         );
         // add details to team array
         allMembers.push(engineer);
-        if (answer.addNew === "Yes") {
-          addNewEmployee();
-        } else {
-          generateHTML();
-        }
+        addTeamMembers();
       });
     } else if (answer.role === "Intern") {
       inquirer.prompt(questions.Intern).then((answer) => {
@@ -284,21 +305,18 @@ function addNewEmployee() {
         );
         // add details to team array
         allMembers.push(intern);
-        if (answer.addNew === "Yes") {
-          addNewEmployee();
-        } else {
-          generateHTML();
-        }
+        addTeamMembers();
       });
     }
   });
 }
 
-addNewEmployee();
-
 // Function to generate HTML
 function generateHTML() {
   fs.writeFileSync(outputPath, render(allMembers), "utf-8");
+  log.red(`
+  === Team Profile Generated ===
+  `);
   process.exit(0);
 }
 
